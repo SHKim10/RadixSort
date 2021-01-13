c0f2ae6636166809b089c01fce76f13ff493c57e
SHKim10 <skim10@stuy.edu>
2021-01-13 10:55:11 -0500

    Modified merge to SortableLinkedList

--- Radix.java
+++ Radix.java
@@ -15,9 +15,11 @@ public class Radix{
     return Math.max(1, answer);
   }
 
-  public static void merge(MyLinkedList original, MyLinkedList[]buckets){
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
     for(int i = 0; i < buckets.length; i++){
       original.extend(buckets[i]);
     }
   }
+
+
 }
